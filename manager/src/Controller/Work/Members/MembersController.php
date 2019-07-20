0259a3dbd1aa2678ae1cb6655f46b83b633b2743
studxxx <stud181177@gmail.com>
2019-07-20 16:22:54 +0300

    Show member projects

--- manager/src/Controller/Work/Members/MembersController.php
+++ manager/src/Controller/Work/Members/MembersController.php
@@ -14,6 +14,7 @@ use App\Model\Work\UseCase\Mebers\Member\Move;
 use App\Model\Work\UseCase\Mebers\Member\Reinstate;
 use App\ReadModel\Work\Members\Member\Filter;
 use App\ReadModel\Work\Members\Member\MemberFetcher;
+use App\ReadModel\Work\Projects\Project\DepartmentFetcher;
 use DomainException;
 use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -219,10 +220,12 @@ class MembersController extends AbstractController
     /**
      * @Route("/{id}", name=".show", requirements={"id"=Guid::PATTERN})
      * @param Member $member
+     * @param DepartmentFetcher $fetcher
      * @return Response
      */
-    public function show(Member $member): Response
+    public function show(Member $member, DepartmentFetcher $fetcher): Response
     {
-        return $this->render('app/work/members/show.html.twig', compact('member'));
+        $departments = $fetcher->allOfMember($member->getId()->getValue());
+        return $this->render('app/work/members/show.html.twig', compact('member', 'departments'));
     }
 }
