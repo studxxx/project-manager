5f5ec3de6e28961dbc983838e6b314eebc54eb59
studxxx <stud181177@gmail.com>
2019-07-21 10:40:34 +0300

    Generated migrations

--- manager/src/Model/Work/Entity/Projects/Task/Task.php
+++ manager/src/Model/Work/Entity/Projects/Task/Task.php
@@ -15,68 +15,96 @@ use Webmozart\Assert\Assert;
 
 /**
  * @ORM\Entity()
- * @ORM\Table()
+ * @ORM\Table(name="work_projects_tasks", indexes={
+ *     @ORM\Index(columns={"date"})
+ * })
  */
 class Task
 {
     /**
      * @var Id
+     * @ORM\Column(type="work_projects_tasks_id")
+     * @ORM\GeneratedValue(strategy="NONE")
+     * @ORM\SequenceGenerator(sequenceName="work_projects_tasks_seq", initialValue=1)
+     * @ORM\Id()
      */
     private $id;
     /**
      * @var Project
+     * @ORM\ManyToOne(targetEntity="App\Model\Work\Entity\Projects\Project\Project")
+     * @ORM\JoinColumn(name="project_id", referencedColumnName="id", nullable=false)
      */
     private $project;
     /**
      * @var Member
+     * @ORM\ManyToOne(targetEntity="App\Model\Work\Entity\Members\Member\Member")
+     * @ORM\JoinColumn(name="author_id", referencedColumnName="id", nullable=false)
      */
     private $author;
     /**
      * @var DateTimeImmutable
+     * @ORM\Column(type="datetime_immutable")
      */
     private $date;
     /**
      * @var string
+     * @ORM\Column(type="string")
      */
     private $name;
     /**
      * @var string|null
+     * @ORM\Column(type="text", nullable=true)
      */
     private $content;
     /**
      * @var Type
+     * @ORM\Column(type="work_projects_tasks_type", length=16)
      */
     private $type;
     /**
      * @var int
+     * @ORM\Column(type="smallint")
      */
     private $progress;
     /**
      * @var int
+     * @ORM\Column(type="smallint")
      */
     private $priority;
     /**
      * @var DateTimeImmutable|null
+     * @ORM\Column(type="datetime_immutable", nullable=true)
      */
     private $planDate;
     /**
-     * @var Task
+     * @var Task|null
+     * @ORM\ManyToOne(targetEntity="Task")
+     * @ORM\JoinColumn(name="parent_id", referencedColumnName="id", nullable=true, onDelete="SET NULL")
      */
     private $parent;
     /**
      * @var Status
+     * @ORM\Column(type="work_projects_tasks_status", length=16)
      */
     private $status;
     /**
      * @var DateTimeImmutable|null
+     * @ORM\Column(type="datetime_immutable", nullable=true)
      */
     private $startDate;
     /**
      * @var DateTimeImmutable|null
+     * @ORM\Column(type="datetime_immutable", nullable=true)
      */
     private $endDate;
     /**
      * @var Member[]|ArrayCollection
+     * @ORM\ManyToMany(targetEntity="App\Model\Work\Entity\Members\Member\Member")
+     * @ORM\JoinTable(name="work_projects_tasks_executors",
+     *     joinColumns={@ORM\JoinColumn(name="task_id", referencedColumnName="id")},
+     *     inverseJoinColumns={@ORM\JoinColumn(name="member_id", referencedColumnName="id")}
+     * )
+     * @ORM\OrderBy({"name.first" = "ASC"})
      */
     private $executors;
 
