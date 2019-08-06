64532ed2f3f32f0ca369d866f746b52d4021b32d
studxxx <stud181177@gmail.com>
2019-08-06 21:48:22 +0300

    Add text decorators
    - htmlpurifier
    - work_processors

---
+++ manager/src/Twig/Extension/Work/Processor/Driver/MemberDriver.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Extension\Work\Processor\Driver;
+
+use App\ReadModel\Work\Members\Member\MemberFetcher;
+use Twig\Environment;
+
+class MemberDriver implements Driver
+{
+    private const PATTERN = '/\@[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/i';
+
+    /** @var MemberFetcher */
+    private $members;
+    /** @var Environment */
+    private $twig;
+
+    public function __construct(MemberFetcher $members, Environment $twig)
+    {
+        $this->members = $members;
+        $this->twig = $twig;
+    }
+
+    public function process(string $text): string
+    {
+        return preg_replace_callback(self::PATTERN, function (array $matches) {
+            $id = ltrim($matches[0], '@');
+            if (!$member = $this->members->find($id)) {
+                return $matches[0];
+            }
+            return $this->twig->render('processor/work/task.html.twig', [
+                'member' => $member
+            ]);
+        }, $text);
+    }
+}
