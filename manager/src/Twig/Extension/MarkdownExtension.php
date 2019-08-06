5d77809ca4476978ba78f03b3b99081952284bfa
studxxx <stud181177@gmail.com>
2019-08-06 20:34:20 +0300

    Add MarkdownExtension

---
+++ manager/src/Twig/Extension/MarkdownExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Extension;
+
+use cebe\markdown\MarkdownExtra;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class MarkdownExtension extends AbstractExtension
+{
+    /** @var MarkdownExtra */
+    private $markdown;
+
+    public function __construct(MarkdownExtra $markdown)
+    {
+        $this->markdown = $markdown;
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('markdown', [$this, 'markdown'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function markdown(?string $text): string
+    {
+        return $this->markdown->parse($text);
+    }
+}
