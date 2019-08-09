ee831ee2d852dbe962521fb7e004a33747f683f5
studxxx <stud181177@gmail.com>
2019-08-09 06:53:59 +0300

    Added file entity

---
+++ manager/src/Model/Work/UseCase/Projects/Task/Files/Add/Command.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Work\UseCase\Projects\Task\Files\Add;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    /**
+     * @var int
+     * @Assert\NotBlank()
+     */
+    public $id;
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     */
+    public $member;
+    /**
+     * @var File[]
+     */
+    public $files;
+
+    public function __construct(int $id, string $member)
+    {
+        $this->id = $id;
+        $this->member = $member;
+    }
+}
