7e5674be97c60291693fda3d309adc7083e89230
studxxx <stud181177@gmail.com>
2019-09-08 07:44:14 +0300

    Added actions lists

---
+++ manager/src/ReadModel/Work/Projects/Action/Filter.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ReadModel\Work\Projects\Action;
+
+class Filter
+{
+    /** @var string|null */
+    public $member;
+    /** @var string|null */
+    public $project;
+
+    public function __construct(?string $project)
+    {
+        $this->project = $project;
+    }
+
+    public static function forProject(string $project): self
+    {
+        return new self($project);
+    }
+
+    public static function all(): self
+    {
+        return new self(null);
+    }
+
+    public function forMember(string $member): self
+    {
+        $clone = clone $this;
+        $clone->member = $member;
+        return $clone;
+    }
+}
